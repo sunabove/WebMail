b2e8d6f4ae03669f09c9b547423f13359313dc34
sunabove <sunabove@nate.com>
2021-01-23 10:23:07 +0900

    DB 스크립트 및 샘플 데이터 입력 스크립트 수정

--- sql_webmail/sql_023_샘플 메일 데이터 입력 02.sql
+++ sql_webmail/sql_023_샘플 메일 데이터 입력 02.sql
@@ -4,72 +4,42 @@ SELECT * FROM n WHERE no < 10 ;
 
 SELECT my_text( 8 ) FROM dual ;
 
+DELETE FROM t_mail WHERE 1 = 1 ; 
 DELETE FROM user WHERE 1 = 1 ;
+
 SELECT * FROM user ;
-INSERT INTO USER (id, name, passwd)
+INSERT INTO t_user (id, name, passwd)
 VALUES
 ( UUID(), 'admin', 'admin' );
 
-INSERT INTO USER (id, name, passwd)
+INSERT INTO t_user (id, name, passwd)
 VALUES
 ( UUID(), 'john', 'admin' ) ;
 
-INSERT INTO user(id, name, passwd) 
+INSERT INTO t_user(id, name, passwd) 
 SELECT UUID(), MY_TEXT(8), MY_TEXT(8) FROM n WHERE no < 100 ;
 
-SELECT * FROM user ;
+SELECT * FROM t_user ;
 
 -- sample mail insert
-DROP TABLE IF EXISTS T_MAIL ;
-
-CREATE TABLE T_MAIL ( 
-MAILID	VARCHAR(40)	PRIMARY KEY NOT NULL COMMENT '메일아이디', 
-TITLE	VARCHAR(1000)		NOT NULL COMMENT '제목', 
-CONTENTFILEID	VARCHAR(40) COMMENT '본문파일아이디',
-UGTYN	CHAR(1) DEFAULT 'N'	COMMENT '긴급여부', 					
-SECURITYYN	CHAR(1) DEFAULT 'N'	COMMENT '보안여부',					
-MAILSIZE	INTEGER(10) NOT NULL COMMENT '메일사이즈',
-EMLHEADERID	VARCHAR(40) COMMENT 'EML헤더아이디',
-EMLFILEID	VARCHAR(40) COMMENT 'EML파일아이디',
-FILEATTACHYN	CHAR(1) DEFAULT 'N'	COMMENT '파일첨부여부',
-IMGATTACHYN	CHAR(1) DEFAULT 'N'	COMMENT '이미지첨부여부',
-ENCODING	VARCHAR(20) COMMENT '인코딩방식',
-DELYN	CHAR(1) DEFAULT 'N' COMMENT '삭제여부',
-MAKEDATE	DATE COMMENT '작성일시',
-SENDDATE	DATE COMMENT '발송일시',
-RCVDATE	DATE COMMENT '수신일시',
-RCVLISTINFO	VARCHAR(300) COMMENT '수신목록정보', 
-RCVUSERID	VARCHAR(200) COMMENT '수신자아이디', 
-RCVTOTCNT	INTEGER(6) DEFAULT 0	COMMENT '수신자열람건수', 
-RCVREADCNT	INTEGER(6) default 0	COMMENT '수신자읽음건수',
-ETCPARAM1	VARCHAR(100) COMMENT '기타속성1',
-ETCPARAM2	VARCHAR(100) COMMENT '기타속성2',
-ETCPARAM3	VARCHAR(100) COMMENT '기타속성3',
-ETCPARAM4	VARCHAR(100) COMMENT '기타속성4',
-ETCPARAM5	VARCHAR(100) COMMENT '기타속성5',
-DEVICE	VARCHAR(20) DEFAULT 'PC'	COMMENT '장치',
-REGUSERID	VARCHAR(40) COMMENT '등록자아이디',
-REGDATE	DATE COMMENT '등록일시', 
-CHGUSERID	VARCHAR(40) COMMENT '변경자아이디',
-CHGDATE	DATE COMMENT '변경일시',
-LARGEYN	CHAR(1) DEFAULT 'N'	COMMENT '대용량메일여부',
-CONVERSIONYN	VARCHAR(10) DEFAULT 'N' COMMENT '컨버젼여부',
-SYSTEMMAILYN	CHAR(1) DEFAULT 'N' , 
-COMMENTCNT	INTEGER(5) DEFAULT 0	COMMENT '댓글개수'  
-) ;
 SELECT COUNT(*) FROM t_mail ;
 DELETE FROM t_mail WHERE 1 = 1 ;
 SELECT * FROM t_mail ;
 
-INSERT INTO t_mail( mailid , title,      mailsize, regUserId, rcvDate, regDate ) 
-SELECT               mailid, title, LENGTH(title),    userId, rcvDate, regDate
+INSERT INTO t_mail( mailId , title,      mailsize, regUserId, rcvDate, regDate ) 
+SELECT               mailId, title, LENGTH(title),    userId, rcvDate, regDate
 FROM ( 
-	SELECT no, UUID() AS mailid, MY_TEXT( 25 ) title, userid,
-	DATE_FORMAT( NOW() - INTERVAL FLOOR(RAND() * 20) DAY , '%Y-%m-%d %H:%i:%s' ) AS rcvDate,
-	DATE_FORMAT( NOW() - INTERVAL FLOOR(RAND() * 20) DAY , '%Y-%m-%d %H:%i:%s' ) AS regDate,
+	SELECT no, UUID() AS mailId, MY_TEXT( 25 ) title, userId,
+	( FROM_UNIXTIME(UNIX_TIMESTAMP(now()) - FLOOR(0 + (RAND() * 2592000))) ) AS rcvDate,
+	NOW() AS regDate,
 	'' AS z
-	FROM n , ( SELECT ROW_NUMBER() over(ORDER BY id) AS userNo, id as userId FROM user ) as user
-	WHERE NO < 10000 AND MOD( NO, 100 ) = userNo 
-) AS a ORDER BY NO  ;
+	FROM n , 
+	( SELECT ( ROW_NUMBER() OVER(ORDER BY id) ) AS userNo, id as userId FROM t_user 
+	) AS t_user
+	WHERE 1 = 1  
+	AND NO < 10000 
+	AND MOD( NO, 100 ) = userNo 
+) AS a 
+ORDER BY NO ;
 
 -- end file
