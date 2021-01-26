a8ecb0c691e8590fbc60125fd2c246a6c3a551be
sunabove <sunabove@nate.com>
2021-01-26 16:52:01 +0900

    쿼리 최적화

--- sql_webmail/200 웹메일 검색 목록.sql
+++ sql_webmail/200 웹메일 검색 목록.sql
@@ -1,27 +1,24 @@
 ﻿
-
 SELECT 
-  FLOOR( rno/row_cnt ) + 1  AS pno,
-  a.*
-FROM ( 
-  SELECT 
-  ROW_NUMBER() OVER(ORDER BY INSTR( tm.title, srch_keyword ), tm.RCVDATE ) rno , 
-  tm.mailid AS mailid , 
-  LAG( tm.mailid  ) OVER( ORDER BY tm.rcvDate ) AS prev_mailid, 
-  LEAD( tm.mailid ) OVER( ORDER BY tm.rcvDate ) AS next_mailid, 
-  tu.userid, tu.name, tm.title ,tms.simplecontent AS content ,
-  tm.RCVUSERID, tm.RCVDATE, tmr.rcvtype , 
-  row_cnt 
-  FROM 
-  ( SELECT 10 AS row_cnt, '' AS srch_title FROM dual ) AS param , 
-  t_user tu
-  LEFT JOIN t_mail tm ON ( tu.userid = tm.rcvUserId AND tu.name='admin' )
-  LEFT JOIN t_mail_rcvinfo tmr ON tm.MAILID = tmr.mailid
-  LEFT JOIN t_mail_simplecontent tms ON tm.MAILID = tms.mailid
-  WHERE 1 = 1 
-  AND tm.mailid IS NOT NULL 
-  AND ( LENGTH( srch_keyword ) = 0 OR SIGN( INSTR( tm.title, srch_keyword ) ) = 1 )
-  ORDER BY INSTR( tm.title, srch_keyword ), tm.RCVDATE 
-  LIMIT 0, 10
-) AS a
-; 
+	  ( FLOOR( rno/row_cnt ) + 1) AS pno,
+	  a.*
+	FROM ( 
+	  SELECT 
+	  ROW_NUMBER() OVER(ORDER BY INSTR( tm.title, srch_keyword ), tm.RCVDATE ) rno , 
+	  tm.mailid AS mailid , 
+	  tu.userid, tu.name, tm.title ,tms.simplecontent AS content ,
+	  tm.RCVUSERID, tm.RCVDATE, tmr.rcvtype , 
+	  row_cnt 
+	  FROM 
+	  ( SELECT 10 AS row_cnt, '' AS srch_keyword FROM dual ) AS param , 
+	  t_user tu
+	  LEFT JOIN t_mail tm ON ( tu.userid = tm.rcvUserId AND tu.name='admin' )
+	  LEFT JOIN t_mail_rcvinfo tmr ON tm.MAILID = tmr.mailid
+	  LEFT JOIN t_mail_simplecontent tms ON tm.MAILID = tms.mailid
+	  WHERE 1 = 1 
+	  AND tm.mailid IS NOT NULL 
+	  AND ( LENGTH( srch_keyword ) = 0 OR SIGN( INSTR( tm.title, srch_keyword ) ) = 1 )
+	  ORDER BY INSTR( tm.title, srch_keyword ), tm.RCVDATE 
+	  LIMIT 0, 10
+	) AS a
+	ORDER BY rno 
